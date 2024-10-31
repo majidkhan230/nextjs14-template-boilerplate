c7a5eddf7aa59dd5a60e0406a7057fbeea7e14d1
MajidKh230 <khanmajid230@gmail.com>
2024-11-01 00:17:26 +0500

    initial commit

---
+++ src/app/(dashboard)/admin/page.jsx
@@ -0,0 +1,14 @@
+import CountChart from "@/components/CountChart"
+
+const Adminpage = () => {
+  return (
+    <div>
+        <div>AdminPage</div>
+        <div>
+          <CountChart/>
+        </div>
+    </div>
+  )
+}
+
+export default Adminpage
---
+++ src/app/signout/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignOutpage = () => {
+  return (
+    <div>SignOutpage</div>
+  )
+}
+
+export default SignOutpage
